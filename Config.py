f1beb0efecc0162e6581bf3ea4bcc5c59575a4ac
Grant Phillips <granticini@users.noreply.github.com>
2018-04-11 20:44:48 +1200

    Handles multiple Log Sources

--- Config.py
+++ Config.py
@@ -1,32 +1,54 @@
 import json
 
+class LogSource(object):
+    def __init__(self, log_source_json, config):
+        self.row_limit = log_source_json.get("row_limit", config.row_limit)
+        self.file_limit = log_source_json.get("file_limit", config.file_limit)
+        
+        self.bucket_name = log_source_json.get("bucket_name", config.bucket_name)
+        self.s3_key_prefix = log_source_json["s3_key_prefix"]
+
+        self.temp_path = log_source_json.get("temp_path", self.s3_key_prefix.split("/")[0])
+        self.log_group = log_source_json["log_group"]
+        self.log_stream_name = log_source_json["log_stream_name"]
+        self.start_after=log_source_json["start_after"]
+        self.s3_object_limit = log_source_json.get("s3_object_limit", config.s3_object_limit)
+
 class Config(object):
     def __init__(self, config_file_name=""):
         if len(config_file_name) == 0:
-            self.config_file_name = "config.json"            
+            self.account_id=""
+            self.config_file_name = "config.json"
             self.row_limit = -1
             self.file_limit = -1 
             self.bucket_name = "BUCKET_NAME"
             self.s3_key_prefix = "/Logs"
             self.log_group = "LOG_GROUP"
             self.log_stream_name="x"
-            self.max_keys=100
+            self.s3_object_limit=100
             self.start_after=""
-            self.data_path="data"
+            self.temp_path="logs-temp"
+            self.log_sources=[]
         else:
-            self.config_file_name = config_file_name
-
             # Read some config from a json file
             print("Loading config")
             with open(config_file_name, 'r') as config_file:
                 config_json = json.loads(config_file.read())
 
-            self.row_limit = config_json["row_limit"]
-            self.file_limit = config_json["file_limit"]
-            self.bucket_name = config_json["bucket_name"]
-            self.s3_key_prefix = config_json["s3_key_prefix"]
-            self.log_group = config_json["log_group"]
-            self.log_stream_name=config_json["log_stream_name"]
-            self.max_keys = config_json["max_keys"]
-            self.start_after=config_json["start_after"]
-            self.data_path = config_json["data_path"]
+            # Header properties
+            self.config_file_name = config_file_name
+            self.start_time = config_json.get("start_time", "")
+            self.end_time = config_json.get("end_time", "")
+
+            self.bucket_name = config_json.get("bucket_name", "")
+
+            self.account_id = config_json.get("account_id", "")
+            self.temp_path = config_json.get("temp_path", "logs-temp")
+            self.row_limit = config_json.get("row_limit", -1)
+            self.file_limit = config_json.get("file_limit", -1)
+            self.s3_object_limit = config_json.get("s3_object_limit", -1)
+            
+            # now loop through each log source
+            self.log_sources = []
+            for log_source_json in config_json["log_sources"]:
+                self.log_sources.append(LogSource(log_source_json, self))
--- import-alb-logs-into-cloudwatch.py
+++ import-alb-logs-into-cloudwatch.py
@@ -76,13 +76,89 @@ def upload_to_cloud_watch_logs(log_group, log_stream, local_file_name):
     # sort the log events
     sorted_events = sort_list_of_dictionaries(log_events, "timestamp")
 
-    print("Uploaading " + str(len(log_events)) + " log events")
-    cloudwatch_logs.put_log_events(
-        logGroupName=log_group,
-        logStreamName=config.log_stream_name, 
-        logEvents = sorted_events,
-        sequenceToken=log_sequence_token)
+    print("Uploading " + str(len(log_events)) + " log events")
+#    cloudwatch_logs.put_log_events(
+#        logGroupName=log_group,
+#        logStreamName=config.log_stream_name, 
+#        logEvents = sorted_events,
+#        sequenceToken=log_sequence_token)
     print("Uploaded " + str(len(sorted_events)) + " records to Cloud Watch Logs")
+
+# -----------------------------
+def process_log_source(log_source):
+    print("Get list of S3 files ...")
+    print("S3 Bucket ....... " + log_source.bucket_name)
+    print("S3 Key Prefix ... " + log_source.s3_key_prefix)
+    print("Start After ..... " + log_source.start_after)
+    print("Max S3 Objects .. " + str(log_source.s3_object_limit))
+
+    print(log_source.__dict__)
+
+    response = s3_client.list_objects_v2(
+        Bucket=log_source.bucket_name,
+        Prefix=log_source.s3_key_prefix,
+        MaxKeys=log_source.s3_object_limit,
+        StartAfter=log_source.start_after
+    )
+
+    file_number = 0
+    contents = response.get("Contents")
+    if contents is None:
+        print("No S3 data files available")
+    else:
+        print("Found " + str(len(contents)) + " S3 files")
+        for item in contents:
+            file_number = file_number + 1
+            if (log_source.file_limit >= 0 and file_number >= log_source.file_limit):
+                break
+
+            s3_key = item["Key"]
+            print("s3 key    = '" + s3_key + "'")
+
+            size = item["Size"]
+            if (size == 0):
+                print("Zero length file, so skip")
+                continue
+
+            if (s3_key[-1] == "/"):
+                print("end with a / character, so skip")
+                continue
+            
+            file_name = s3_key.split("/")[-1]
+            print("file_name = '" + file_name + "'")
+            local_file_name = log_source.temp_path + "/" + file_name
+
+            if os.path.isfile(local_file_name):
+                print("Already downloaded")
+            else:
+                print("Downloading file from S3 ...")
+                ok = False
+                try:
+                    s3_client.download_file(
+                        Bucket=log_source.bucket_name,
+                        Key=s3_key,
+                        Filename=local_file_name)
+                    ok = True
+                except botocore.exceptions.ClientError as e:
+                    ok = False
+                    if e.response['Error']['Code'] == "404":
+                        print("The object does not exist.")
+                    else:
+                        raise
+
+                if ok:
+                    print("Downloaded ok")
+                else:
+                    print("Problem with download")
+                    continue
+
+            # now upload the data into Cloud Watch Logs
+            upload_to_cloud_watch_logs(log_source.log_group, log_source.log_stream_name, local_file_name)
+
+            # update config for next time
+            log_source.start_after = s3_key
+            with open(config.config_file_name, 'w') as config_file:
+                config_file.write(json.dumps(config.__dict__))
 
 # ------------------------------------------
 DBG("Import ALB/ELB Logs From S3 into Cloud Watch Logs")
@@ -92,88 +168,20 @@ s3_client = boto3.client('s3')
 cloudwatch = boto3.client("cloudwatch")
 cloudwatch_logs = boto3.client("logs")
 
-config = Config("config.json")
+config = Config("config-prod1.json")
 config.start_time = time.strftime('%Y-%m-%d %H:%M:%S')
 print(config.start_time)
 
-if not os.path.exists(config.data_path):
-    print ("Create the data path\n" + config.data_path)
-    os.makedirs(config.data_path)
+if not os.path.exists(config.temp_path):
+    print ("Create the data path\n" + config.temp_path)
+    os.makedirs(config.temp_path)
 
-# keep track of where we are up to
-# so we don't get the same files again.
-print("Get list of S3 files ...")
-print("S3 Key Prefix ... " + config.s3_key_prefix)
-print("Start After ..... " + config.start_after)
-response = s3_client.list_objects_v2(
-    Bucket=config.bucket_name,
-    Prefix=config.s3_key_prefix,
-    MaxKeys=config.max_keys,
-    StartAfter=config.start_after
-)
-
-file_number = 0
-contents = response.get("Contents")
-if contents is None:
-    print("No S3 data files available")
-else:
-    print("Found " + str(len(contents)) + " S3 files")
-    for item in contents:
-        if (config.file_limit >= 0 and file_number >= config.file_limit):
-            break
-
-        s3_key = item["Key"]
-        print("s3 key    = '" + s3_key + "'")
-
-        size = item["Size"]
-        if (size == 0):
-            print("Zero length file, so skip")
-            continue
-
-        if (s3_key[-1] == "/"):
-            print("end with a / character, so skip")
-            continue
-        
-        file_number = file_number + 1
-        file_name = s3_key.split("/")[-1]
-        print("file_name = '" + file_name + "'")
-        local_file_name = config.data_path + "/" + file_name
-
-        if os.path.isfile(local_file_name):
-            print("Already downloaded")
-        else:
-            print("Downloading file from S3 ...")
-            ok = False
-            try:
-                s3_client.download_file(
-                    Bucket=config.bucket_name,
-                    Key=s3_key,
-                    Filename=local_file_name)
-                ok = True
-            except botocore.exceptions.ClientError as e:
-                ok = False
-                if e.response['Error']['Code'] == "404":
-                    print("The object does not exist.")
-                else:
-                    raise
-
-            if ok:
-                print("Downloaded ok")
-            else:
-                print("Problem with download")
-                continue
-
-        # now upload the data into Cloud Watch Logs
-        upload_to_cloud_watch_logs(config.log_group, config.log_stream_name, local_file_name)
-
-        # update config for next time
-        config.start_after = s3_key
-        with open(config.config_file_name, 'w') as config_file:
-            config_file.write(json.dumps(config.__dict__))
-
+# Process each Log Source
+for log_source in config.log_sources:
+    process_log_source(log_source)
 
 config.end_time = time.strftime('%Y-%m-%d %H:%M:%S')
-print(json.dumps(config.__dict__))
+print(config)
 
 # update config for next time
 with open(config.config_file_name, 'w') as config_file:
